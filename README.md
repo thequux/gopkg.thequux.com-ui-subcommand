94689e86ae012ab5fb6e286babc204acb6782d6e
Dan Hirsch <thequux@upstandinghackers.com>
2013-03-11 21:01:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+gopkg.thequux.com/ui/subcommand
+=============================
+
+Provides an interface to git-style subcommands
+
+See example/ for an example of how it can be used.
---
+++ framework.go
@@ -0,0 +1,73 @@
+package subcommand
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var (
+	registry =  map[string]Module{}
+)
+
+type Module struct {
+	Flags *flag.FlagSet
+	Main func()
+	Name string
+	Descr string
+}
+
+func usage() {
+	fmt.Printf("Usage:\n  %s [global options] subcommand [subcommand arguments]\n\n", os.Args[0])
+
+	
+	modules := make([]string, 0, len(registry))
+	for name, _ := range registry {
+		modules = append(modules, name)
+	}
+	sort.StringSlice(modules).Sort()
+	fmt.Printf("Available commands:\n")
+	for _, name := range modules {
+		fmt.Printf("\t%s\t%s\n", name, registry[name].Descr)
+	}
+
+	fmt.Printf("\nGlobal options:\n")
+	flag.PrintDefaults()
+}
+
+func init() {
+	flag.Usage = usage
+}
+
+func Register(m Module) {
+	registry[m.Name] = m
+}
+
+func Subcommand(cmd string) *Module {
+	if mod, exist := registry[cmd]; exist {
+		return &mod
+	}
+	return nil
+	
+}
+
+// Convenience function that can be called directly from main
+func Main() {
+	flag.Parse()
+
+	if flag.NArg() == 0 {
+		flag.Usage()
+		return
+	}
+
+	submod := Subcommand(flag.Arg(0))
+	if submod == nil {
+		fmt.Printf("%s %s: subcommand not found\n", os.Arg(0), flag.Arg(0))
+		flag.Usage()
+		return
+	}
+
+	submod.Flags.Parse(flag.Args()[1:])
+	submod.Main()
+}
